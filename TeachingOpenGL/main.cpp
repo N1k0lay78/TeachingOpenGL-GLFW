f54ea685b0a30c876c8c94f6d57b5e4ea25bf57a
Rjkzavr <kolya.toropof@gmail.com>
2021-03-08 22:47:21 +0300

    Creating clear window
    include GLAD
    add window initialisation and any collback

--- TeachingOpenGL/main.cpp
+++ TeachingOpenGL/main.cpp
@@ -1,37 +1,76 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(void)
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// Константы
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
 {
-	GLFWwindow* window;
+	// glfw: инициализация и конфигурирование
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // раскомментируйте эту строку, если используете macOS
+#endif
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(1920, 1080, "NikiZavr Super Puper Mega Game!!!", NULL, NULL);
-	if (!window)
+	// glfw: создание окна
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Rjkzavr application", NULL, NULL);
+	if (window == NULL)
 	{
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	/* Loop until the user closes the window */
+	// glad: загрузка всех указателей на OpenGL-функции
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Цикл рендеринга
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
+		// Обработка ввода
+		processInput(window);
+
+		// Выполнение рендеринга
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		/* Swap front and back buffers */
+		// glfw: обмен содержимым front- и back- буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
+	// glfw: завершение, освобождение всех ранее задействованных GLFW-ресурсов
 	glfwTerminate();
 	return 0;
+}
+
+// Обработка всех событий ввода: запрос GLFW о нажатии/отпускании клавиш на клавиатуре в данном кадре и соответствующая обработка данных событий
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: всякий раз, когда изменяются размеры окна (пользователем или операционной системой), вызывается данная callback-функция
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// Убеждаемся, что окно просмотра соответствует новым размерам окна. 
+	// Обратите внимание, что ширина и высота будут значительно больше, чем указано, на Retina-дисплеях
+	glViewport(0, 0, width, height);
 }
